b07132c5bb29f65bff3481b308eaf7215910e90f
saniyafarzindeveloper <developerfarzin@gmail.com>
2025-03-17 21:43:55 +0530

    re-arranged folder structure

--- routes/user.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { login, logout, register } from '../controllers/user.js';
-const router = express.Router();
-
-router.route("/").post(register);
-router.route("/login").post(login);
-router.route("/logout").get(logout)
-
-export default router;
